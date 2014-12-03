92215fc9553a13477bbb21bed4065242aa51c129
Christopher Primerano <cprimera@mac.com>
2014-12-03 10:54:00 -0500

    removed stuff that potentially didn't work and wasn't completely necessary

--- src/Chart.MultiFloatingBar.js
+++ src/Chart.MultiFloatingBar.js
@@ -1,231 +1,128 @@
-(function(){
-	"use strict";
+/*
+ * Chart.js
+ * http://chartjs.org/
+ * Version: 0.1.0-beta.0
+ *
+ * Copyright 2014 Christopher Primerano
+ * Released under the MIT license
+ * https://github.com/cprimera/Chart.MultiFloatingBar.js/blob/master/LICENSE
+ */
 
-	var root = this,
-		Chart = root.Chart,
-		helpers = Chart.helpers;
+(function () {
+
+	/* global Chart */
+	var helpers = Chart.helpers;
 
 	var defaultConfig = {
-		scaleBeginAtZero : true,
-
-		//Boolean - Whether grid lines are shown across the chart
-		scaleShowGridLines : true,
-
-		//String - Colour of the grid lines
-		scaleGridLineColor : "rgba(0,0,0,.05)",
-
-		//Number - Width of the grid lines
-		scaleGridLineWidth : 1,
-
-		//Boolean - If there is a stroke on each bar
-		barShowStroke : true,
-
-		//Number - Pixel width of the bar stroke
-		barStrokeWidth : 2,
-
-		//Number - Spacing between each of the X value sets
-		barValueSpacing : 5,
-
-		//Boolean - Whether bars should be rendered on a percentage base
-		relativeBars : false,
-
 		//String - A legend template
-		legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"><%if(datasets[i].label){%><%=datasets[i].label%><%}%></span></li><%}%></ul>"
-
+		legendTemplate : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].fillColor%>"><%if(datasets[i].label){%><%=datasets[i].label%><%}%></span></li><%}%></ul>'
 	};
 
-	Chart.Type.extend({
-		name: "MultiFloatingBar",
+	Chart.types.Bar.extend({
+		
+		name: 'MultiFloatingBar',
 		defaults : defaultConfig,
 		initialize:  function(data){
-			//Expose options as a scope variable here so we can access it in the ScaleClass
+
 			var options = this.options;
 
 			this.ScaleClass = Chart.Scale.extend({
-				offsetGridLines : true,
-				calculateBarX : function(barIndex){
+				offsetGridLines: true,
+				calculateBarX: function (barIndex) {
 					return this.calculateX(barIndex);
 				},
-				calculateBarY : function(datasets, dsIndex, barIndex, value){
-					var offset = 0,
-						sum = 0;
-
-					// for(var i = 0; i < datasets.length; i++) {
-					// 	sum += datasets[i].bars[barIndex].value;
-					// }
-					// for(i = dsIndex; i < datasets.length; i++) {
-					// 	if(i === dsIndex && value) {
-					// 		offset += value;
-					// 	} else {
-					// 		offset += datasets[i].bars[barIndex].value;
-					// 	}
-					// }
-
-					// if(options.relativeBars) {
-					// 	offset = offset / sum * 100;
-					// }
-
-					return this.calculateY(value);
+				calculateBaseWidth: function () {
+					return (this.calculateX(1) - this.calculateX(0)) - (2 * options.barValueSpacing);
 				},
-				calculateBaseWidth : function(){
-					return (this.calculateX(1) - this.calculateX(0)) - (2*options.barValueSpacing);
-				},
-				calculateBaseHeight : function(){
-					return (this.calculateY(1) - this.calculateY(0));
-				},
-				calculateBarWidth : function(datasetCount){
-					//The padding between datasets is to the right of each bar, providing that there are more than 1 dataset
+				calculateBarWidth: function (datasetCount) {
 					return this.calculateBaseWidth();
-				},
-				calculateBarHeight : function(datasets, dsIndex, barIndex, value) {
-					var sum = 0;
-
-					// for(var i = 0; i < datasets.length; i++) {
-					// 	sum += datasets[i].bars[barIndex].value;
-					// }
-
-					// if(!value) {
-					// 	value = datasets[dsIndex].bars[barIndex].value;
-					// }
-
-					// if(options.relativeBars) {
-					// 	value = value / sum * 100;
-					// }
-
-					return this.calculateY(datasets[dsIndex].bars[barIndex].endValue - value);
 				}
 			});
 
-			this.datasets = [];
-
-			//Set up tooltip events on the chart
 			if (this.options.showTooltips){
-				helpers.bindEvents(this, this.options.tooltipEvents, function(evt){
-					var activeBars = (evt.type !== 'mouseout') ? this.getBarsAtEvent(evt) : [];
+				helpers.bindEvents(this, this.options.tooltipEvents, function(e){
+					var toolTipBars = (e.type !== 'mouseout') ? this.getBarsAtEvent(e) : [];
 
-					this.eachBars(function(bar){
+					this.eachBars(function (bar) {
 						bar.restore(['fillColor', 'strokeColor']);
 					});
-					helpers.each(activeBars, function(activeBar){
-						activeBar.fillColor = activeBar.highlightFill;
-						activeBar.strokeColor = activeBar.highlightStroke;
+
+					helpers.each(toolTipBars, function(toolTipBar){
+						toolTipBar.fillColor = toolTipBar.highlightFill;
+						toolTipBar.strokeColor = toolTipBar.highlightStroke;
 					});
-					this.showTooltip(activeBars);
+					this.showTooltip(toolTipBars);
 				});
 			}
 
-			//Declare the extension of the default point, to cater for the options passed in to the constructor
+			this.datasets = [];
+
 			this.BarClass = Chart.Rectangle.extend({
-				strokeWidth : this.options.barStrokeWidth,
-				showStroke : this.options.barShowStroke,
-				ctx : this.chart.ctx
+				strokeWidth: this.options.barStrokeWidth,
+				showStroke: this.options.barShowStroke,
+				ctx: this.chart.ctx
 			});
 
-			//Iterate through each of the datasets, and build this into a property of the chart
-			helpers.each(data.datasets,function(dataset,datasetIndex){
-
+			helpers.each(data.datasets, function (dataset, datasetIndex) {
 				var datasetObject = {
-					label : dataset.label || null,
-					fillColor : dataset.fillColor,
-					strokeColor : dataset.strokeColor,
-					bars : []
+					label: dataset.label || null,
+					fillColor: dataset.fillColor,
+					strokeColor: dataset.strokeColor,
+					bars: []
 				};
 
 				this.datasets.push(datasetObject);
 
-				helpers.each(dataset.data,function(dataPoint,index){
-					if (helpers.isNumber(dataPoint.start) && helpers.isNumber(dataPoint.end)){
-						//Add a new point for each piece of data, passing any required data to draw.
-						datasetObject.bars.push(new this.BarClass({
-							startValue : dataPoint.start,
-							endValue: dataPoint.end,
-							value: dataPoint.start,
-							label : data.labels[index],
-							datasetLabel: dataset.label,
-							strokeColor : dataset.strokeColor,
-							fillColor : dataset.fillColor,
-							highlightFill : dataset.highlightFill || dataset.fillColor,
-							highlightStroke : dataset.highlightStroke || dataset.strokeColor
-						}));
-					}
-				},this);
+				helpers.each(dataset.data, function (dataPoint, dataPointIndex) {
+					var bar = new this.BarClass({
+						startValue: dataPoint.start,
+						endValue: dataPoint.end,
+						value: dataPoint.start,
+						label: data.labels[dataPointIndex],
+						datasetLabel: dataset.label,
+						fillColor: dataset.fillColor,
+						strokeColor: dataset.strokeColor,
+						highlightFill: dataset.highlightFill || dataset.fillColor,
+						highlightStroke: dataset.highlightStroke || dataset.strokeColor
+					});
 
-			},this);
+					datasetObject.bars.push(bar);
+
+				}, this);
+
+			}, this);
 
 			this.buildScale(data.labels);
 
-			this.eachBars(function(bar, index, datasetIndex){
+
+			this.eachBars(function (bar, barIndex) {
 				helpers.extend(bar, {
 					base: this.scale.endPoint,
-					height: 0,
-					width : this.scale.calculateBarWidth(this.datasets.length),
-					x: this.scale.calculateBarX(index),
-					y: this.scale.endPoint
+					x: this.scale.calculateBarX(barIndex),
+					y: this.scale.endPoint,
+					width: this.scale.calculateBarWidth(this.datasets.length)
 				});
 				bar.save();
 			}, this);
 
 			this.render();
+			
 		},
-		update : function(){
-			this.scale.update();
-			// Reset any highlight colours before updating.
-			helpers.each(this.activeElements, function(activeElement){
-				activeElement.restore(['fillColor', 'strokeColor']);
-			});
+		buildScale: function (labels) {
 
-			this.eachBars(function(bar){
-				bar.save();
-			});
-			this.render();
-		},
-		eachBars : function(callback){
-			helpers.each(this.datasets,function(dataset, datasetIndex){
-				helpers.each(dataset.bars, callback, this, datasetIndex);
-			},this);
-		},
-		getBarsAtEvent : function(e){
-			var barsArray = [],
-				eventPosition = helpers.getRelativePosition(e),
-				datasetIterator = function(dataset){
-					barsArray.push(dataset.bars[barIndex]);
-				},
-				barIndex;
-
-			for (var datasetIndex = 0; datasetIndex < this.datasets.length; datasetIndex++) {
-				for (barIndex = 0; barIndex < this.datasets[datasetIndex].bars.length; barIndex++) {
-					var x = this.datasets[datasetIndex].bars[barIndex].x;
-					var y = this.datasets[datasetIndex].bars[barIndex].y;
-					var width = this.datasets[datasetIndex].bars[barIndex].width;
-					var base = this.datasets[datasetIndex].bars[barIndex].base;
-					if (eventPosition.x >= x - (width / 2) && eventPosition.x <= x + (width /2) && eventPosition.y >= base && eventPosition.y <= y) {
-						helpers.each(this.datasets, datasetIterator);
-						return barsArray;
-					}
-					if (this.datasets[datasetIndex].bars[barIndex].inRange(eventPosition.x,eventPosition.y)){
-						helpers.each(this.datasets, datasetIterator);
-						return barsArray;
-					}
-				}
-			}
-
-			return barsArray;
-		},
-		buildScale : function(labels){
 			var self = this;
 
-			var dataTotal = function(){
+			var dataTotal = function () {
 				var values = [];
-				helpers.each(self.datasets, function(dataset) {
-					helpers.each(dataset.bars, function(bar, barIndex) {
-						if(!values[barIndex]) values[barIndex] = 0;
-						if(self.options.relativeBars) {
-							values[barIndex] = 100;
-						} else {
-							values[barIndex] = (values[barIndex] < bar.startValue) ? bar.startValue : values[barIndex];
-						}
-					});
+
+				self.eachBars(function (bar, barIndex) {
+					if (!values[barIndex]) {
+						values[barIndex] = 0;
+					}
+
+					values[barIndex] = (values[barIndex] < bar.startValue) ? bar.startValue : values[barIndex];
 				});
+
 				return values;
 			};
 
@@ -256,7 +153,7 @@
 				lineWidth : this.options.scaleLineWidth,
 				lineColor : this.options.scaleLineColor,
 				gridLineWidth : (this.options.scaleShowGridLines) ? this.options.scaleGridLineWidth : 0,
-				gridLineColor : (this.options.scaleShowGridLines) ? this.options.scaleGridLineColor : "rgba(0,0,0,0)",
+				gridLineColor : (this.options.scaleShowGridLines) ? this.options.scaleGridLineColor : 'rgba(0,0,0,0)',
 				padding : (this.options.showScale) ? 0 : (this.options.barShowStroke) ? this.options.barStrokeWidth : 0,
 				showLabels : this.options.scaleShowLabels,
 				display : this.options.showScale
@@ -274,74 +171,49 @@
 
 			this.scale = new this.ScaleClass(scaleOptions);
 		},
-		addData : function(valuesArray,label){
-			//Map the values array for each of the datasets
-			helpers.each(valuesArray,function(value,datasetIndex){
-				if (helpers.isNumber(value.start) && helpers.isNumber(value.end)){
-					//Add a new point for each piece of data, passing any required data to draw.
-					this.datasets[datasetIndex].bars.push(new this.BarClass({
-						startValue : value.start,
-						endValue: value.end,
-						label : label,
-						x: this.scale.calculateBarX(this.scale.valuesCount+1),
-						y: this.scale.endPoint,
-						width : this.scale.calculateBarWidth(this.datasets.length),
-						base : this.scale.endPoint,
-						strokeColor : this.datasets[datasetIndex].strokeColor,
-						fillColor : this.datasets[datasetIndex].fillColor
-					}));
-				}
-			},this);
+		update: function () {
+			this.scale.update();
 
-			this.scale.addXLabel(label);
-			//Then re-render the chart.
-			this.update();
+			helpers.each(this.activeElements, function (activeElement, elementIndex) {
+				activeElement.restore(['fillColor', 'strokeColor']);
+			});
+
+			this.eachBars(function (bar) {
+				bar.save();
+			});
+
+			this.render();
 		},
-		removeData : function(){
-			this.scale.removeXLabel();
-			//Then re-render the chart.
-			helpers.each(this.datasets,function(dataset){
-				dataset.bars.shift();
-			},this);
-			this.update();
-		},
-		reflow : function(){
-			helpers.extend(this.BarClass.prototype,{
+		reflow: function () {
+			helpers.extend(this.BarClass.prototype, {
 				y: this.scale.endPoint,
-				base : this.scale.endPoint
+				base: this.scale.endPoint
 			});
+
 			var newScaleProps = helpers.extend({
-				height : this.chart.height,
-				width : this.chart.width
+				height: this.chart.height,
+				width: this.chart.width
 			});
+
 			this.scale.update(newScaleProps);
 		},
-		draw : function(ease){
-			var easingDecimal = ease || 1;
+		draw: function(ease) {
+			ease = ease || 1;
+
 			this.clear();
 
-			var ctx = this.chart.ctx;
+			this.scale.draw(ease);
 
-			this.scale.draw(easingDecimal);
-
-			//Draw all the bars for each dataset
-			helpers.each(this.datasets,function(dataset,datasetIndex){
-				helpers.each(dataset.bars,function(bar,index){
-					var y = this.scale.calculateBarY(this.datasets, datasetIndex, index, bar.endValue),
-						height = this.scale.calculateBarHeight(this.datasets, datasetIndex, index, bar.startValue);
-
-					//Transition then draw
+			this.eachBars(function (bar, barIndex) {
+				if (bar.startValue !== bar.endValue) {
 					bar.transition({
-						base: this.scale.calculateY(bar.endValue),
-						// base : this.scale.endPoint - (Math.abs(height) - Math.abs(y)),
-						x : this.scale.calculateBarX(index),
-						y: this.scale.calculateY(bar.startValue),
-						// y : Math.abs(y),
-						// height : Math.abs(height),
-						width : this.scale.calculateBarWidth(this.datasets.length)
-					}, easingDecimal).draw();
-				},this);
-			},this);
+						base: this.scale.calculateY(bar.startValue),
+						x: this.scale.calculateX(barIndex),
+						y: this.scale.calculateY(bar.endValue),
+						width: this.scale.calculateBarWidth(this.datasets.length)
+					}, ease).draw();
+				}
+			}, this);
 		}
 	});
 }).call(this);
